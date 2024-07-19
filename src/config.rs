4cd03f77f65b83f03eba939f26551c32d3f8ff9e
Hans Askov <hans@askov.dk>
2024-07-19 10:04:04 +0200

    better cli interface

--- src/config.rs
+++ src/config.rs
@@ -3,17 +3,12 @@ use clap::Parser;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
-    #[arg(
-        short,
-        long,
-        env,
-        default_value = "postgres://username:password@localhost:5432/mydatabase"
-    )]
+    #[arg(short, long = "db-url", env)]
     pub database_url: String,
 
-    #[arg(short, long, env, default_value = "[::0]:50051")]
-    pub server_addr: String,
+    #[arg(short, long ="addres", env, default_value = "[::0]:50051")]
+    pub server_addres: String,
 
-    #[arg(short, long, env, default_value = "5")]
+    #[arg(short, long , env, default_value = "5")]
     pub max_connections: u32,
 }
